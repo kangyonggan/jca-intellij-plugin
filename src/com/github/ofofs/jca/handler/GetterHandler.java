e0dd5ba1315fc93eb6aa15dfa6a178f4a42fb77d
kangyonggan <kangyonggan@gmail.com>
2018-07-09 11:32:56 +0800

    init

---
+++ src/com/github/ofofs/jca/handler/GetterHandler.java
@@ -0,0 +1,19 @@
+package com.github.ofofs.jca.handler;
+
+import com.intellij.codeInsight.CodeInsightActionHandler;
+import com.intellij.openapi.editor.Editor;
+import com.intellij.openapi.project.Project;
+import com.intellij.psi.PsiFile;
+import org.jetbrains.annotations.NotNull;
+
+/**
+ * @author kangyonggan
+ * @since 7/9/18
+ */
+public class GetterHandler implements CodeInsightActionHandler {
+
+    @Override
+    public void invoke(@NotNull Project project, @NotNull Editor editor, @NotNull PsiFile psiFile) {
+
+    }
+}
